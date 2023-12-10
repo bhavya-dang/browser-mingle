924d453835874dc5112b5290a24909c471ce0aef
jailbreakerVC <54243183+jailbreakerVC@users.noreply.github.com>
2023-12-10 22:11:47 +0530

    added supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import supabase from "../supabase";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  async function test() {
+    let { data: test, error } = await supabase.from("test").select("id");
+    console.log(test, error);
+  }
+  useEffect(() => {
+    test();
+  }, []);
+
+  console.log();
 
   return (
     <>
@@ -29,7 +39,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://rvcsutokdfgfytaugjyw.supabase.co";
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
