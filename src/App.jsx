3dfc6dd9367ea3316fecdcef56229078976c5493
s-m33r <112017240+s-m33r@users.noreply.github.com>
2023-12-15 01:07:09 +0530

    small refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import MessageList from "./components/MessageList";
+import MessageInput from "./components/MessageInput";
 import supabase from "../supabase";
 
-function App() {
+const App = () => {
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
   const [lusername, _] = useState(localStorage.getItem("username"));
-  const [topic, setTopic] = useState(""); // room topic
-  const [room, setRoom] = useState(""); // room, controlled by getRoomId
-
+  const [topic, setTopic] = useState("");
+  const [room, setRoom] = useState("");
+  
   // Function to send a message
   async function sendMessage() {
     const message = input;
@@ -21,28 +22,28 @@ function App() {
           username: lusername,
         },
       ]);
-
+  
       if (error) {
         console.error("Error sending message:", error.message);
       }
-
+  
       setInput("");
       document.getElementById("input-box").value = "";
     }
   }
-
+  
   async function addMessage(msgobj) {
     setMessages((prevMessages) => [...prevMessages, msgobj]);
   }
-
+  
   async function getRoomId(name) {
     const { data, error } = await supabase.functions.invoke("join-room", {
       body: { topic: name },
     });
-
+  
     return data.room_id;
   }
-
+  
   useEffect(() => {
     const subscription = supabase
       .channel("messages")
@@ -60,49 +61,16 @@ function App() {
       )
       .subscribe();
   }, [room]);
-
+  
   return (
     <>
-      <div className="fixed bottom-10 pb-7 justify-center left-2 right-2  h-auto overflow-y-auto">
-        {messages.map((msg, index) => {
-          const dateObject = new Date(msg.timestamp);
-          const formattedTime = `${dateObject.getHours()}:${dateObject.getMinutes()}`;
-          return (
-            <div className="flex items-center mb-2" key={index}>
-              <div className="ml-3 text-left">
-                <div className="text-sm font-medium text-black">
-                  {msg.username}{" "}
-                  <span className="text-gray-400">at {formattedTime}</span>
-                </div>
-                <div className="text-black ">
-                  <span className="font-medium font-small ">{msg.content}</span>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <div className="flex fixed bottom-3 left-2 right-2 gap-3 justify-center   ">
-        <input
-          type="text"
-          className="input input-bordered input-primary w-full max-w-xs rounded-md"
-          name="message"
-          id="input-box"
-          onChange={(e) => setInput(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key == "Enter") {
-              sendMessage();
-            }
-          }}
-          placeholder="say something"
-        />
-        <button
-          className="btn btn-neutral rounded-md"
-          onClick={() => sendMessage()}
-        >
-          Send
-        </button>
-      </div>
+      <MessageList messages={messages} />
+      <MessageInput
+        input={input}
+        setInput={setInput}
+        sendMessage={sendMessage}
+      />
+  
       <br />
       <input
         type="text"
@@ -117,7 +85,7 @@ function App() {
         }}
         placeholder="room topic"
       />
-
+  
       <p>room id: {room}</p>
     </>
   );
---
+++ src/components/MessageInput.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const MessageInput = ({ input, setInput, sendMessage }) => {
+  return (
+    <div className="flex fixed bottom-3 left-2 right-2 gap-3 justify-center">
+      <input
+        type="text"
+        className="input input-bordered input-primary w-full max-w-xs rounded-md"
+        name="message"
+        id="input-box"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            sendMessage();
+          }
+        }}
+        placeholder="say something"
+      />
+      <button
+        className="btn btn-neutral rounded-md"
+        onClick={() => sendMessage()}
+      >
+        Send
+      </button>
+    </div>
+  );
+};
+
+export default MessageInput;
+
